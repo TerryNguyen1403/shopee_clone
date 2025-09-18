bcd337410be047c38141cb5d6bcbbc6d1d1dc7a3
TerryNguyen1403 <giangnht1403@gmail.com>
2025-09-18 14:48:24 +0700

    Refactor Navbar and adding all-products route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,23 +2,25 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // Import components
-import NavBar from "./components/NavBar";
+import NavbarWrapper from "./components/NavComponents/NavbarWrapper";
 
 // Import pages
-import Home from './pages/Home/Home.tsx';
+import Home from './pages/Home';
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Cart from './pages/Cart';
+import AllProducts from './pages/AllProducts'
 
 const App: React.FC = () => {
     return (
         <BrowserRouter>
-            <NavBar />
+            <NavbarWrapper />
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Login />}/>
                 <Route path="/register" element={<Register />}/>
                 <Route path="/cart" element={<Cart />}/>
+                <Route path="/all-products" element={<AllProducts />}/>
             </Routes>
         </BrowserRouter>
     )
